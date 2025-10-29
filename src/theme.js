eba6fea31766bc4b186867e02fd94ea1cbe8f745
Agbajestephen <agbajestephen5@gmail.com>
2025-10-30 00:06:46 +0100

    update

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "rgba(23, 96, 165, 1)",
+      light: "skyblue",
+    },
+    secondary: {
+      main: "#15c630",
+    },
+    otherColor: {
+      main: "rgba(153, 153, 153, 1)",
+    },
+  },
+});
