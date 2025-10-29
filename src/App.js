53ed1a10163b2cb5e12dd9eb89cc95d625539f48
Agbajestephen <agbajestephen5@gmail.com>
2025-10-30 00:05:28 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar"
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Stack } from "@mui/material";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Box>
+      <Navbar/>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
